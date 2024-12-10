0b3a8ef5c068843ef2ce0e556fee31b2b16dcff9
0533132458l <lh24582458@gmail.com>
2024-12-10 12:06:19 +0200

    add Delete Student function

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -6,6 +6,10 @@ import { Student } from '../student.model';
   templateUrl: './student-list.component.html'
 })
 export class StudentListComponent {
-  
+
     students:Student[]=[{id:1,name:"student1"},{id:2,name:"student2"},{id:3,name:"student3"}]
+    deleteStudent(student:Student){
+      let indexToDelete=this.students.indexOf(student);
+      this.students.splice(indexToDelete,1);
+    }
 }
